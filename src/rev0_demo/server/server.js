75ff38cee458a7e46cabe20b71a77074a576eb8e
fardeenAfsar <afsarfardeen@gmail.com>
2024-01-28 01:42:50 -0500

    Set up initial mongo db connection

--- src/rev0_demo/server/server.js
+++ src/rev0_demo/server/server.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://housemates783:Capstone100@housematesapp.jecbapz.mongodb.net/?retryWrites=true&w=majority'
 
 // Example of api request and response. TODO: Delete later
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne"]})
 })
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+// Connect to DB
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(PORT, () => {
+            console.log("Server started on port", PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
