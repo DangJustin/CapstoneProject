fe6098427bfe41a086fcc01516b3c8c9e1e9a30e
DangJustin <justin.dang6024@gmail.com>
2024-02-04 11:12:12 -0500

    Added stubs for functions and routes

--- src/rev0_demo/server/controllers/taskManagementController.js
+++ src/rev0_demo/server/controllers/taskManagementController.js
@@ -1,4 +1,38 @@
 exports.index = async (req, res, next) => {
-    console.log("Task Management");
+    console.log("Task Management index");
     res.status(200).send("This is the index for the Task Management module");
   }
+
+exports.addTask = async (req, res, next) => {
+  console.log("Task Management addTask");
+  res.status(200).send("This is the addTasks for the Task Management module");
+  //TODO
+}
+
+exports.getTasks = async (req, res, next) => {
+  console.log("Task Management getTasks");
+  res.status(200).send("This is the getTasks for the Task Management module");
+  //TODO
+}
+
+exports.getTask = async (req, res, next) => {
+  const taskID = req.params.id;
+  console.log("Task Management getTask: " + String(taskID));
+  res.status(200).send("This is the getTask for the Task Management module, taskID: " + String(taskID));
+  //TODO
+}
+
+exports.editTask = async (req, res, next) => {
+  const taskID = req.params.id;
+  console.log("Task Management editTask: " + String(taskID));
+  res.status(200).send("This is the editTask for the Task Management module, taskID: " + String(taskID));
+  //TODO
+}
+
+exports.completeTask = async (req, res, next) => {
+  const taskID = req.params.id;
+  console.log("Task Management completeTask: " + String(taskID));
+  res.status(200).send("This is the completeTask for the Task Management module, taskID: " + String(taskID));
+  //TODO
+}
+
---
+++ src/rev0_demo/server/services/taskManagementService.js
@@ -0,0 +1,16 @@
+export function getTask(taskID) {
+    // TODO
+}
+
+export function getTasks(userID){
+    // TODO
+}
+
+export function addTask(taskParameters){
+    //TODO
+}
+
+export function completeTask(taskID){
+    //TODO
+}
+
