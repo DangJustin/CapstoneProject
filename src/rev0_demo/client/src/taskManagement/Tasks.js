b0909a7175a06bcd7561b3655fced9f6a7cf602a
DangJustin <justin.dang6024@gmail.com>
2024-02-04 10:35:29 -0500

    Added template way to show tasks on tasks page

--- src/rev0_demo/client/src/taskManagement/Tasks.js
+++ src/rev0_demo/client/src/taskManagement/Tasks.js
@@ -4,15 +4,53 @@ import { useNavigate } from 'react-router-dom';
 
 function Tasks() {
   const [user,setUser] = useState([]);
+  const [tasks,setTasks] = useState([{id:1, name:'task1', complete:'No'},{id:2, name:'task2',complete:'Yes'}]);
   const navigate = useNavigate();
-  const goToTask = (id) => {
-    navigate('task/'+id);
-  };
+ 
   useEffect(() => {},[]);
+
+
+  const handleSelectChange = (event) =>{
+    navigate('task/'+String(event.target.value));
+  };
+
   return (
     <div>
       <h1>Tasks Page</h1>
-      <button onClick={() => goToTask(30)}>Test task</button>
+      
+      {/*Table to show all tasks*/}
+      <div>
+      <table border="1">
+        <thead>
+            <tr>
+                <th>Task ID</th>
+                <th>Task Name</th>
+                <th>Completion Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            {tasks.map((task) => {
+                return(
+                    <tr key={task.id}>
+                        <td>{task.id}</td>
+                        <td>{task.name}</td>
+                        <td>{task.complete}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+        </table>
+      </div>
+
+      {/*Dropdown menu to select task*/}
+      <div>
+      <select value={""} onChange={handleSelectChange}>
+        <option value="">Select a task</option>
+        {tasks.map(task => (
+          <option key={task.id} value={task.id}>{task.name}</option>
+        ))}
+      </select>
+      </div>
     </div>
   );
 }
