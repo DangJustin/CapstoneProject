ee50e50eedb5c5f38fc8e7538ee8215f53e7b312
Rizwan Ahsan <rizahsan4@gmail.com>
2024-02-05 02:35:39 -0500

    Added addTask function

--- src/rev0_demo/server/services/taskManagementService.js
+++ src/rev0_demo/server/services/taskManagementService.js
@@ -1,16 +1,37 @@
-export function getTask(taskID) {
+const Task = require('../models/taskModel');
+
+function getTask(taskID) {
     // TODO
 }
 
-export function getUserTasks(userID){
+function getUserTasks(userID){
     // TODO
 }
 
-export function addTask(taskParameters){
+async function addTask(taskName, groupID, deadlineDate, description){
+    try {
+        // Create a new task instance
+        const newTask = new Task({
+          taskName,
+          groupID,
+          deadlineDate,
+          description,
+        });
+    
+        // Save the new task to the database
+        const savedTask = await newTask.save();
+    
+        // Return the saved task or perform additional actions
+        return savedTask;
+      } catch (error) {
+        // Handle any errors during the task creation
+        console.error('Error adding task:', error);
+        throw error; // You may want to handle errors in a more specific way
+      }
+}
+
+function completeTask(taskID){
     //TODO
 }
 
-export function completeTask(taskID){
-    //TODO
-}
-
+module.exports = { getTask, getUserTasks, addTask, completeTask };
