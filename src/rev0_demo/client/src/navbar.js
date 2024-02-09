7abf4602294f1c23db734aea150578c3a8ab09e3
Rizwan Ahsan <rizahsan4@gmail.com>
2024-02-09 18:58:11 -0500

    Fix hamburger menu bug closes #86

--- src/rev0_demo/client/src/navbar.js
+++ src/rev0_demo/client/src/navbar.js
@@ -60,39 +60,54 @@ function Navbar() {
     return (
         <div className="bootstrap-scope">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <a className="navbar-brand" href="#">Housemates</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/taskManagement">Tasks</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/scheduling">Schedules</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/billManagement">Bills</a>
-                    </li>
-                </ul>
-                </div>
-                {currentUser && <span className="navbar-text me-2">Hello, {userInfo.email}</span>}
-                <div className="d-flex">
-                    {currentUser && (
+
+                <div className="container">
+                    <a className="navbar-brand" href="#">Housemates</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/taskManagement">Tasks</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/scheduling">Schedules</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/billManagement">Bills</a>
+                        </li>
+                        </ul>
+                        
+                        <div className="d-lg-none">
+                            {currentUser && <span className="navbar-text me-2">Hello, {userInfo.email}</span>}
+                            {currentUser && (
+                                <button className="btn btn-outline-primary me-2" type="button" onClick={goToAccount}>Account</button>
+                            )}
+                            {currentUser ? (
+                                <button className="btn btn-outline-secondary" type="button" onClick={handleLogout}>Log Out</button>
+                            ) : (
+                                <button className="btn btn-outline-secondary" type="button" onClick={handleLogout}>Log In</button>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="d-none d-lg-flex ">
+                        {currentUser && <span className="navbar-text me-2">Hello, {userInfo.email} </span>}
+                        {currentUser && (
                         <button className="btn btn-outline-primary me-2" type="button" onClick={goToAccount}>Account</button>
-                    )}
-                    {currentUser ? (
+                        )}
+                        {currentUser ? (
                         <button className="btn btn-outline-secondary" type="button" onClick={handleLogout}>Log Out</button>
-                    ) : (
+                        ) : (
                         <button className="btn btn-outline-secondary" type="button" onClick={handleLogout}>Log In</button>
-                    )}               
+                        )}
+                    </div>
                 </div>
-            </div>
             </nav>
         </div>
     );
