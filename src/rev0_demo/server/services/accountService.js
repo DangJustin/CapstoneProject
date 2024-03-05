04d93b1cce09fd84d26c953385657e3a222f90c4
Fady Morcos <fadymorcos22@gmail.com>
2024-03-05 16:47:34 -0500

    beautified

--- src/rev0_demo/server/services/accountService.js
+++ src/rev0_demo/server/services/accountService.js
@@ -2,65 +2,65 @@ const User = require("../models/userModel");
 const Group = require("../models/groupModel");
 
 // Method to find groups for a specific user
-async function getUserGroups(userID){
+async function getUserGroups(userID) {
     try {
         // Find User
         const user = await User.findOne({ userID: userID });
-        if (!user){
+        if (!user) {
             throw new Error('User with ID: ' + String(userID) + ' Not found')
         }
         // Retrieve the user's groups
         const userGroups = await Group.find({ users: user._id });
         return userGroups;
-    } catch (error){
+    } catch (error) {
         console.error('Error trying to find group:', error);
-        throw error; // You may want to handle errors in a more specific way
+        throw error;
     }
 }
 
 // Method to find specific user based off user.userID
-async function getUser(userID){
+async function getUser(userID) {
     try {
         // Find User
         const user = await User.findOne({ userID: userID });
-        if (!user){
+        if (!user) {
             throw new Error('User with ID: ' + String(userID) + ' Not found')
         }
         return user;
-    } catch (error){
+    } catch (error) {
         console.error('Error trying to find group:', error);
-        throw error; // You may want to handle errors in a more specific way
+        throw error;
     }
 }
 
 // Method to find specific user based off user._id
-async function getUserOffID(userID){
+async function getUserOffID(userID) {
     try {
         // Find User
         const user = await User.findById(userID);
-        if (!user){
+        if (!user) {
             throw new Error('User with ID: ' + String(userID) + ' Not found')
         }
         return user;
-    } catch (error){
+    } catch (error) {
         console.error('Error trying to find group:', error);
-        throw error; // You may want to handle errors in a more specific way
+        throw error;
     }
 }
 
 // Method to find specific group based off group._id
-async function getGroup(groupID){
+async function getGroup(groupID) {
     try {
         // Find User
         const group = await Group.findById(groupID);
-        if (!group){
+        if (!group) {
             throw new Error('Group with ID: ' + String(groupID) + ' Not found')
         }
         return group;
-    } catch (error){
+    } catch (error) {
         console.error('Error trying to find group:', error);
-        throw error; // You may want to handle errors in a more specific way
+        throw error;
     }
 }
 
-module.exports = {getUserGroups,getUser,getUserOffID,getGroup};
+module.exports = { getUserGroups, getUser, getUserOffID, getGroup };
