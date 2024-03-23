134c1864d114351a270a2ed6aec052f8a559e41a
HamidHarris <harrishamid1990@gmail.com>
2024-03-23 16:57:59 -0400

    UI improvement to billmanagement

--- src/rev0_demo/client/src/billManagement/BillManagement.js
+++ src/rev0_demo/client/src/billManagement/BillManagement.js
@@ -14,6 +14,7 @@ function BillManagement() {
   const [settlingDebt, setSettlingDebt] = useState(null);
   const [settlementAmount, setSettlementAmount] = useState(0);
   const [updatedUserAmount, setUpdatedUserAmount] = useState(userAmount);
+  const [showSettlementModal, setShowSettlementModal] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -57,6 +58,7 @@ function BillManagement() {
   const handleSettle = async (user, amount) => {
     try {
       setSettlingDebt(user);
+      setShowSettlementModal(true);
     } catch (error) {
       console.error("Error Settling", error)
     }
@@ -67,7 +69,12 @@ function BillManagement() {
     
       if (settlingDebt && settlementAmount) {
         const updatedDebts = { ...debts };
-        updatedDebts[settlingDebt] -= settlementAmount;
+        if (updatedUserAmount >= 0){
+          updatedDebts[settlingDebt] -= settlementAmount;
+        }
+        else {
+          updatedDebts[settlingDebt] += settlementAmount;
+        }
         setDebts(updatedDebts);
         if (updatedUserAmount >= 0) {
           setUpdatedUserAmount(updatedUserAmount - settlementAmount);
@@ -83,6 +90,7 @@ function BillManagement() {
         owingUserEmail: settlingDebt
       };
       setSettlingDebt(null);
+      setShowSettlementModal(false);
       // setSettlementAmount(0);
 
       await axios.put(`http://localhost:5000/api/database/updateUserAmount/${currentUser.uid}`,
@@ -101,36 +109,153 @@ function BillManagement() {
         <hr></hr>
         <button className="btn btn-primary mt-3" onClick={goToAddExpense}>Add Expense</button>
         <button className="btn btn-secondary mt-3 mx-3" onClick={goToViewExpenses}>View Expense</button>
-
+  
         <h1 className="mt-3">
-          {updatedUserAmount < 0 ? `Overall, you owe $${updatedUserAmount.toFixed(2)}` : `Overall, you are owed $${updatedUserAmount.toFixed(2)}`}
+          {updatedUserAmount < 0 ? `Overall, you owe $${Math.abs(updatedUserAmount).toFixed(2)}` : `Overall, you are owed $${updatedUserAmount.toFixed(2)}`}
         </h1>
         <h2>Interpersonal Debt Relations:</h2>
-        <ul className="list-group">
+        
+        <div className="row row-cols-1 row-cols-md-2 g-4">
           {Object.entries(debts).filter(([user, amount]) => user !== currentUser.email).map(([user, amount]) => (
-            <li key={user} className="list-group-item d-flex justify-content-between align-items-center">
-              {amount < 0
-                ? `You owe user ${user} $${Math.abs(amount).toFixed(2)}`
-                : `User ${user} owes you $${Math.abs(amount).toFixed(2)}`}
-              <button className="btn btn-outline-danger" onClick={() => handleSettle(user, amount)}>Settle</button>
-            </li>
+            <div key={user} className="col">
+              <div className={`card ${amount < 0 ? "bg-danger" : "bg-success"} text-white`}>
+                <div className="card-body d-flex justify-content-between align-items-center">
+                  <div>
+                    <h5 className="card-title">
+                      {amount < 0 ? `You owe user ${user}` : `User ${user} owes you`}
+                    </h5>
+                    <p className="card-text">
+                      {amount < 0 ? `$${Math.abs(amount).toFixed(2)}` : `$${Math.abs(amount).toFixed(2)}`}
+                    </p>
+                  </div>
+                  <button className="btn btn-outline-light" onClick={() => handleSettle(user, amount)}>Settle</button>
+                </div>
+              </div>
+            </div>
           ))}
-        </ul>
-
-        {/* Input for settlement amount */}
-        {settlingDebt && (
-          <div className="mt-3">
-            <input
-              type="number"
-              value={settlementAmount}
-              onChange={(e) => setSettlementAmount(parseFloat(e.target.value))}
-              className="form-control"
-            />
-            <button className="btn btn-success mt-2" onClick={handleSettleExpense} disabled={settlementAmount > Math.abs(debts[settlingDebt])}>Settle Expense</button>
+        </div>
+  
+        {/* Modal for settlement amount */}
+        {settlingDebt && showSettlementModal && (
+          <div className="modal fade show" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
+            <div className="modal-backdrop fade show"></div>
+              <div className="modal-dialog modal-dialog-centered" role="document" style={{ zIndex: 1050 }}>
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Settle Expense</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => setShowSettlementModal(false)}>
+                      {/* <span aria-hidden="true">&times;</span> */}
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                <input
+                  type="number"
+                  value={settlementAmount}
+                  onChange={(e) => setSettlementAmount(parseFloat(e.target.value))}
+                  className="form-control"
+                />
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => setShowSettlementModal(false)}>Close</button>
+                <button type="button" className="btn btn-success" onClick={handleSettleExpense} disabled={settlementAmount > Math.abs(debts[settlingDebt])}>Settle Expense</button>
+              </div>
+            </div>
           </div>
-        )}
+        </div>
+)}
+
       </div>
     </Layout>
   );
+
+  // return (
+  //   <Layout>
+  //     <div className="container">
+  //       <h1 className="text-center pt-3">Bill Management Page</h1>
+  //       <hr></hr>
+  //       <button className="btn btn-primary mt-3" onClick={goToAddExpense}>Add Expense</button>
+  //       <button className="btn btn-secondary mt-3 mx-3" onClick={goToViewExpenses}>View Expense</button>
+  
+  //       <h1 className="mt-3">
+  //         {updatedUserAmount < 0 ? `Overall, you owe $${updatedUserAmount.toFixed(2)}` : `Overall, you are owed $${updatedUserAmount.toFixed(2)}`}
+  //       </h1>
+  //       <h2>Interpersonal Debt Relations:</h2>
+        
+  //       <div className="row row-cols-1 row-cols-md-2 g-4">
+  //         {Object.entries(debts).filter(([user, amount]) => user !== currentUser.email).map(([user, amount]) => (
+  //           <div key={user} className="col">
+  //             <div className={`card ${amount < 0 ? "bg-warning" : "bg-success"} text-white`}>
+  //               <div className="card-body d-flex justify-content-between align-items-center">
+  //                 <div>
+  //                   <h5 className="card-title">
+  //                     {amount < 0 ? `You owe user ${user}` : `User ${user} owes you`}
+  //                   </h5>
+  //                   <p className="card-text">
+  //                     {amount < 0 ? `$${Math.abs(amount).toFixed(2)}` : `$${Math.abs(amount).toFixed(2)}`}
+  //                   </p>
+  //                 </div>
+  //                 <button className="btn btn-outline-light" onClick={() => handleSettle(user, amount)}>Settle</button>
+  //               </div>
+  //             </div>
+  //           </div>
+  //         ))}
+  //       </div>
+  
+  //       {/* Input for settlement amount */}
+  //       {settlingDebt && (
+  //         <div className="mt-3">
+  //           <input
+  //             type="number"
+  //             value={settlementAmount}
+  //             onChange={(e) => setSettlementAmount(parseFloat(e.target.value))}
+  //             className="form-control"
+  //           />
+  //           <button className="btn btn-success mt-2" onClick={handleSettleExpense} disabled={settlementAmount > Math.abs(debts[settlingDebt])}>Settle Expense</button>
+  //         </div>
+  //       )}
+  //     </div>
+  //   </Layout>
+  // );
+  
+
+
+  // return (
+  //   <Layout>
+  //     <div className="container">
+  //       <h1 className="text-center pt-3">Bill Management Page</h1>
+  //       <hr></hr>
+  //       <button className="btn btn-primary mt-3" onClick={goToAddExpense}>Add Expense</button>
+  //       <button className="btn btn-secondary mt-3 mx-3" onClick={goToViewExpenses}>View Expense</button>
+
+  //       <h1 className="mt-3">
+  //         {updatedUserAmount < 0 ? `Overall, you owe $${updatedUserAmount.toFixed(2)}` : `Overall, you are owed $${updatedUserAmount.toFixed(2)}`}
+  //       </h1>
+  //       <h2>Interpersonal Debt Relations:</h2>
+  //       <ul className="list-group">
+  //         {Object.entries(debts).filter(([user, amount]) => user !== currentUser.email).map(([user, amount]) => (
+  //           <li key={user} className="list-group-item d-flex justify-content-between align-items-center">
+  //             {amount < 0
+  //               ? `You owe user ${user} $${Math.abs(amount).toFixed(2)}`
+  //               : `User ${user} owes you $${Math.abs(amount).toFixed(2)}`}
+  //             <button className="btn btn-outline-danger" onClick={() => handleSettle(user, amount)}>Settle</button>
+  //           </li>
+  //         ))}
+  //       </ul>
+
+  //       {/* Input for settlement amount */}
+  //       {settlingDebt && (
+  //         <div className="mt-3">
+  //           <input
+  //             type="number"
+  //             value={settlementAmount}
+  //             onChange={(e) => setSettlementAmount(parseFloat(e.target.value))}
+  //             className="form-control"
+  //           />
+  //           <button className="btn btn-success mt-2" onClick={handleSettleExpense} disabled={settlementAmount > Math.abs(debts[settlingDebt])}>Settle Expense</button>
+  //         </div>
+  //       )}
+  //     </div>
+  //   </Layout>
+  // );
 }
 export default BillManagement;
